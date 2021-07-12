40b1ca5a51db0a854eccfbd2351b08322b4c59f0
Vishal Vrk <vishalvrk97@gmail.com>
2021-07-12 14:12:07 +0530

    made necessary changes

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -17,6 +17,16 @@
 import './commands'
 
 import 'cypress-xpath'
+import addContext from 'mochawesome/addContext'
+
+Cypress.on("test:after:run", (test, runnable) => {
+    
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
